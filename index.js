a319d0f98819ce561216b7a6251b24a7d6d838f7
Jocelyn <jocelynvargas123@yahoo.com>
2023-03-05 13:42:23 -0800

    lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function introduction(name) {
-    return (`Hi my name is ${name}.`)
+    return (`Hi, my name is ${name}.`)
 }
 introduction(`Aki`)
 introduction(`Samip`)
@@ -11,7 +11,7 @@ introductionWithLanguage("Aki", "Ember.js")
 introductionWithLanguage("Samip", "React")
 
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return (`Hi, my name is ${name} and Iam learning to program in ${language}.`)
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
 }
 introductionWithLanguageOptional("Gracie")
 introductionWithLanguageOptional("Gracie", "Python")
